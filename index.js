6fb7b2363a81996e3f8ad57cae777c4b88312377
morsaline <morsalinecoding@gmail.com>
2022-11-24 04:25:49 +0600

    some reasone to delete node module

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -7,10 +8,33 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.t17zvb5.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const phonesStall = client.db("phonesStall");
+    const usersCollection = phonesStall.collection("users");
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch((err) => console.log(err));
+
 app.get("/", (req, res) => {
   console.log("server running");
 });
 
-app.port(port, () => {
+app.listen(port, () => {
   console.log(port);
 });

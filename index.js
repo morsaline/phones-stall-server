b20901f76802beb2cb772da8e4705ad601da5c30
morsaline <morsalinecoding@gmail.com>
2022-11-25 22:38:09 +0600

    buyer delete success

--- index.js
+++ index.js
@@ -141,6 +141,12 @@ async function run() {
       const result = await usersCollection.deleteOne(query);
       res.send(result);
     });
+    app.delete("/buyer/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }

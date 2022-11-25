0ca8a80afaf9396ed25ddc4ecf83a4beb7a73bda
morsaline <morsalinecoding@gmail.com>
2022-11-26 04:36:26 +0600

    add payment

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
 app.use(cors());
 app.use(express.json());
@@ -22,6 +23,7 @@ async function run() {
     const usersCollection = phonesStall.collection("users");
     const phonesCollection = phonesStall.collection("phonesCollection");
     const bookingsCollection = phonesStall.collection("bookings");
+    const paymentsCollection = phonesStall.collection("payments");
 
     app.put("/users", async (req, res) => {
       const user = req.body;
@@ -95,6 +97,18 @@ async function run() {
       const result = await usersCollection.findOne(query);
       res.send(result);
     });
+    app.get("/orders", async (req, res) => {
+      const query = {};
+      const result = await bookingsCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.get("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await bookingsCollection.findOne(query);
+      res.send(result);
+    });
+
     //my orders
     app.get("/myorders/:email", async (req, res) => {
       const email = req.params.email;
@@ -121,6 +135,21 @@ async function run() {
       res.send(result);
     });
 
+    ///Payment
+    app.post("/create-payment-intent", async (req, res) => {
+      const order = req.body;
+      const price = order.price;
+      const amount = price * 100;
+      const paymentintent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({
+        clientSecret: paymentintent.client_secret,
+      });
+    });
+
     app.get("/allsellers", async (req, res) => {
       const filter = { role: { $in: ["seller"] } };
       const cursor = usersCollection.find(filter);
@@ -135,13 +164,13 @@ async function run() {
       res.send(result);
     });
 
-    app.delete("/seller/:id", async (req, res) => {
+    app.delete("/user/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await usersCollection.deleteOne(query);
       res.send(result);
     });
-    app.delete("/buyer/:id", async (req, res) => {
+    app.delete("/user/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await usersCollection.deleteOne(query);

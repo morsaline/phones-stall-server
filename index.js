a7e90dd7d0b1cb0302e7a80b3a109e59a2fb2a6b
morsaline <morsalinecoding@gmail.com>
2022-11-26 07:04:22 +0600

    add jwt

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
@@ -46,10 +47,28 @@ async function run() {
       );
       res.send(result);
     });
+
+    app.get("/jwt", async (req, res) => {
+      const email = req.query.email;
+      const query = { email };
+
+      const user = await usersCollection.findOne(query);
+
+      if (user) {
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, {
+          expiresIn: "1d",
+        });
+        return res.send({ accessToken: token });
+      }
+      // console.log(user);
+      res.status(403).send({ accessToken: "" });
+    });
+
     app.get("/users", async (req, res) => {
       const email = req.query.email;
 
       let query = {};
+
       if (email) {
         query = {
           email: email,
